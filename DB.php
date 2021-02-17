d09517352271cc02ebb21abd9ed889c2e40af67d
idei-test <gg.hiro.zen@gmail.com>
2021-02-17 18:17:42 +0900

    ㏈説明、整形

--- DB.php
+++ DB.php
@@ -2,64 +2,74 @@
 // このクラスのなかにpdoクラスを所持させる
 // 外部にたいしてデータベースにアクセスしやすいメソッド提供する
 // mvcのモデルクラスもどきをめざす
-class DB{
+class DB
+{
     private  $db;
-    function __construct(){
-        $this->db = new PDO("mysql:dbname=quiz;host=localhost","root","");
+    function __construct()
+    {
+        $this->db = new PDO("mysql:dbname=quiz;host=localhost", "root", "");
     }
-    
+
     // 新しくユーザー追加
-    public function insertUser(array $data){
+    public function insertUser(array $data)
+    {
         $stmt = $this->db->prepare(
-            "INSERT INTO quiz_user(name,password,email)values(?,?,?)" );
-        $stmt ->bindValue(1,$data['name']);
-        $stmt ->bindValue(2,$data['password']);
-        $stmt ->bindValue(3,$data['mail']);
+            "INSERT INTO quiz_user(name,password,email)values(?,?,?)"
+        );
+        $stmt->bindValue(1, $data['name']);
+        $stmt->bindValue(2, $data['password']);
+        $stmt->bindValue(3, $data['mail']);
         $stmt->execute();
-        $db=NULL;
+        $db = NULL;
     }
-    public function hasUser(string $mail,string $pass){
+    // bool 指定のユーザーはいるか
+    public function hasUser(string $mail, string $pass)
+    {
         $stmt = $this->db->prepare(
-            "SELECT * FROM quiz_user WHERE email=? AND password=?");
-        $stmt->bindValue(1,$mail);
-        $stmt->bindValue(2,$pass);
+            "SELECT * FROM quiz_user WHERE email=? AND password=?"
+        );
+        $stmt->bindValue(1, $mail);
+        $stmt->bindValue(2, $pass);
         $stmt->execute();
-        $db=NULL;
-        if($stmt->fetchAll(PDO::FETCH_ASSOC)){
+        $db = NULL;
+        if ($stmt->fetchAll(PDO::FETCH_ASSOC)) {
             return true;
         }
         return false;
-        }
-        public function getUser(string $mail,string $pass){
+    }
+    // user 表示
+    public function getUser(string $mail, string $pass)
+    {
         $stmt = $this->db->prepare(
-            "SELECT * FROM quiz_user WHERE email=? AND password=?");
-        $stmt->bindValue(1,$mail);
-        $stmt->bindValue(2,$pass);
+            "SELECT * FROM quiz_user WHERE email=? AND password=?"
+        );
+        $stmt->bindValue(1, $mail);
+        $stmt->bindValue(2, $pass);
         $stmt->execute();
-        $db=NULL;
+        $db = NULL;
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
     }
     //------------クイズの操作
-    public function getQuizTitle(){
+    // 全体から　タイトルとidを取得
+    public function getQuizTitle()
+    {
         $stmt = $this->db->prepare(
             "SELECT title,quiz_id FROM quiz_data"
         );
         $stmt->execute();
-        $db=NULL;
+        $db = NULL;
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    public function getQuiz(string $number){
+    // idを指定して　クイズを検索
+    public function getQuiz(string $number)
+    {
         $stmt = $this->db->prepare(
             "SELECT * FROM quiz_data WHERE quiz_id=?"
         );
-        $stmt->bindValue(1,$number);
+        $stmt->bindValue(1, $number);
         $stmt->execute();
-        $db=NULL;
+        $db = NULL;
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
 }
-
-?>
